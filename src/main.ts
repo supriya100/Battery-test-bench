c22815b76de4a3840ee7dfc4c828d265117b9f5e
supriya100 <66246726+supriya100@users.noreply.github.com>
2024-12-19 06:46:13 +0000

    main ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+ import { HttpClient } from '@angular/common/http';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
